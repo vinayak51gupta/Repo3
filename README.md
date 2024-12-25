c441285304c29d3aed3c76345c71e91d301ae7c2
vinayak51gupta <vinayak51gupta@gmail.com>
2024-12-25 18:07:09 +0530

    fixed bugs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Repo3
+
+Author - vinayak Gupta
---
+++ repo3.java
@@ -0,0 +1,6 @@
+package Repo3;
+    public class repo3{
+        public static void main(String args[]){
+            System.out.print("repo3");
+        }
+    }
